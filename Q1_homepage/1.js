79b45e5d52bae9e3fdc6239f5ed11c38b70bbc51
madhavtank <madhav.tank@students.iiit.ac.in>
2022-05-03 13:03:36 +0530

    question 1

---
+++ Q1_homepage/1.js
@@ -0,0 +1,182 @@
+function myFunction()
+{   
+document.body.style.backgroundColor= "green";
+document.body.style.backgroundImage="none";
+}
+function myFunction1()
+{   
+document.body.style.backgroundColor= "red";
+document.body.style.backgroundImage="none";
+}
+function myFunction2()
+{   
+document.body.style.backgroundColor= "blue";
+document.body.style.backgroundImage="none";
+}
+function myFunction3()
+{   
+document.body.style.backgroundColor= "purple";
+document.body.style.backgroundImage="none";
+}
+function myFunction4()
+{   
+document.body.style.backgroundImage="url('https://images.unsplash.com/photo-1483232539664-d89822fb5d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80')";
+}
+function increaseFontSizeBy5px() {
+    txt = document.getElementById("abcd1");
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize + 5) + 'px';
+    txt = document.getElementById("abcd2");
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize + 5) + 'px';
+    txt = document.getElementById("abcd3");
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize + 5) + 'px';
+    txt = document.getElementById("abcd3");
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize + 5) + 'px';
+    txt = document.getElementById("abcd4");
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize + 5) + 'px';
+    txt = document.getElementById("abcd5");
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize + 5) + 'px';
+    txt = document.getElementById("abcd6");
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize + 5) + 'px';
+    txt = document.getElementById("abcd7");
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize + 5) + 'px';
+    txt = document.getElementById("abcd8");
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize + 5) + 'px';
+    txt = document.getElementById("abcd9");
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize + 5) + 'px';
+    txt = document.getElementById("abcd10");
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize + 5) + 'px';
+    txt = document.getElementById("abcd11");
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize + 5) + 'px';
+    txt = document.getElementById("abcd12");
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize + 5) + 'px';
+}
+function decreaseFontSizeBy4px() {
+    txt = document.getElementById("abcd1");
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize - 4) + 'px';
+    txt = document.getElementById("abcd2");
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize - 4) + 'px';
+    txt = document.getElementById("abcd3");
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize - 4) + 'px';
+    txt = document.getElementById("abcd4");
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize - 4) + 'px';
+    txt = document.getElementById("abcd5");
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize - 4) + 'px';
+    txt = document.getElementById("abcd6");
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize - 4) + 'px';
+    txt = document.getElementById("abcd7");
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize - 4) + 'px';
+    txt = document.getElementById("abcd8");
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize - 4) + 'px';
+    txt = document.getElementById("abcd9");
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize - 4) + 'px';
+    txt = document.getElementById("abcd10");
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize - 4) + 'px';
+    txt = document.getElementById("abcd11");
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize - 4) + 'px';
+    txt = document.getElementById("abcd12");
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize - 4) + 'px';
+}
+
+function default_font() {
+    document.getElementById("abcd1").style.fontSize = "20px";
+    document.getElementById("abcd2").style.fontSize = "35px";
+    document.getElementById("abcd3").style.fontSize = "35px";
+    document.getElementById("abcd4").style.fontSize = "20px";
+    document.getElementById("abcd5").style.fontSize = "35px";
+    document.getElementById("abcd6").style.fontSize = "20px";
+    document.getElementById("abcd7").style.fontSize = "35px";
+    document.getElementById("abcd8").style.fontSize = "20px";
+    document.getElementById("abcd9").style.fontSize = "35px";
+    document.getElementById("abcd10").style.fontSize = "35px";
+    document.getElementById("abcd11").style.fontSize = "20px";
+    document.getElementById("abcd12").style.fontSize = "20px";
+}
+var updates_form;
+
+window.onload = function()
+{
+	updates_form = document.getElementById("updates_form");
+	if( updates_form )
+	setupupdates();
+}
+    var updatetable, totalupdates, abc;
+
+function setupupdates()
+{
+	totalupdates = parseInt(window.localStorage.getItem("madhav:totalupdates"));
+	if( !totalupdates )
+		totalupdates = 0;
+	updatetable = document.getElementById("newupdates");
+	var row, cell;
+	for(var i=1; i<=totalupdates; i++)
+	{
+		abc = window.localStorage.getItem("madhav:" + i);
+		abc = JSON.parse(abc);
+		row = updatetable.insertRow();
+		cell = row.insertCell();
+		cell.innerHTML = abc.name;
+		cell = row.insertCell();
+		cell.innerHTML = abc.updates;
+	}
+}
+
+function updatesubmit()
+{
+	var abc = {
+	name: document.forms["updates_form"]["name"].value,
+	updates: document.forms["updates_form"]["updates"].value
+	}
+	window.localStorage.setItem("madhav:totalupdates", totalupdates + 1);
+	window.localStorage.setItem("madhav:" + (totalupdates+1), JSON.stringify(abc));
+}
+
